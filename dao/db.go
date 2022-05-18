9b1fe1e7e26a91219156b5954b0681899e574804
Hanekawa-san <captainduck.official@gmail.com>
2022-05-18 14:46:47 +0300

    roles are ready

--- dao/db.go
+++ dao/db.go
@@ -3,16 +3,15 @@ package dao
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
-
-const dsn = os.Getenv("ELEPHANT")
 
 type DB struct {
 	*gorm.DB
 }
 
 func New() (*DB, error) {
-	gormDB, err := gorm.Open(postgres.Open(dsn))
+	gormDB, err := gorm.Open(postgres.Open(os.Getenv("ELEPHANT")))
 	if err != nil {
 		return nil, err
 	}
